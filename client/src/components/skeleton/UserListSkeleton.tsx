ef3e3b86f66cc9136319152c84c16d01559a8b1d
arunraj.r <arunraj.r@nergyindia.com>
2025-09-09 12:24:32 +0530

    basic new pages

--- client/src/components/skeleton/UserListSkeleton.tsx
+++ client/src/components/skeleton/UserListSkeleton.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function UserListSkeleton() {
   return (
-    <div className="flex items-center animate-pulse rounded-md shadow-sm overflow-hidden hover:shadow-md transition-shadow border border-gray-100">
-      <div className="w-[130px] h-[130px] flex-none shrink-0 bg-gray-200"></div>
+    <div className="flex items-center animate-pulse overflow-hidden">
+      <div className="w-[130px] h-[130px] flex-none shrink-0 bg-gray-200 rounded-full"></div>
       <div className="py-3 px-5 flex-auto space-y-2">
         <div className="w-full h-4 bg-gray-200 rounded-md"></div>
         <div className="w-full h-4 bg-gray-200 rounded-md"></div>
